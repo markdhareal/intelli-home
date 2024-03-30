5a7f2ca0d9c747d6122002084a1a92efce8efe35
Dharyl <programmingdha@gmail.com>
2024-03-30 10:43:08 +0800

    Separate the functions of track_hands and finger_counter

--- hand_tracker/hand_tracker.py
+++ hand_tracker/hand_tracker.py
@@ -7,9 +7,24 @@ class HandTracker:
         self.mp_draw = mp.solutions.drawing_utils
         self.mp_hand = mp.solutions.hands
 
+    def finger_counter(self,list_for_land_mark):
+        tip_ids = [4,8,12,16,20]
+        count_fingers = []
+        if len(list_for_land_mark) != 0:
+            if list_for_land_mark[tip_ids[0]][1] > list_for_land_mark[tip_ids[0]-1][1]:
+                count_fingers.append(1)
+            else:
+                count_fingers.append(0)
+            for id in range(1,5):
+                if list_for_land_mark[tip_ids[id]][2] < list_for_land_mark[tip_ids[id]-2][2]:
+                    count_fingers.append(1)
+                else:
+                    count_fingers.append(0)
+
+            total_fingers = count_fingers.count(1)
+            print(total_fingers)
+
     def track_hands(self):
-         
-         tip_ids = [4,8,12,16,20]
 
          cap = cv2.VideoCapture(0)
 
@@ -27,26 +42,15 @@ class HandTracker:
                 if results.multi_hand_landmarks:
                     for hand_landmark in results.multi_hand_landmarks:
                         my_hands = results.multi_hand_landmarks[0]
-                        for id, land_mark in enumerate(my_hands.landmark):
+                        for self.id, land_mark in enumerate(my_hands.landmark):
                             h, w, c = image.shape
                             coordinate_x, coordinate_y = int(land_mark.x * w), int(land_mark.y * h)
                             land_mark_list.append([id, coordinate_x, coordinate_y])
                         self.mp_draw.draw_landmarks(image, hand_landmark, self.mp_hand.HAND_CONNECTIONS)
 
-                count_fingers = []
-                if len(land_mark_list) != 0:
-                    if land_mark_list[tip_ids[0]][1] > land_mark_list[tip_ids[0]-1][1]:
-                        count_fingers.append(1)
-                    else:
-                        count_fingers.append(0)
-                    for id in range(1,5):
-                        if land_mark_list[tip_ids[id]][2] < land_mark_list[tip_ids[id]-2][2]:
-                            count_fingers.append(1)
-                        else:
-                            count_fingers.append(0)
+                
+                self.finger_counter(land_mark_list)
 
-                    total_fingers = count_fingers.count(1)
-                    print(total_fingers)
                 cv2.imshow("Frame", image)
                 k = cv2.waitKey(1)
 
@@ -55,3 +59,5 @@ class HandTracker:
         
          cap.release()
          cv2.destroyAllWindows()
+
+    
