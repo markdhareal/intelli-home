d9542667789995478800947502875bccbcb5afdd
Dharyl <programmingdha@gmail.com>
2024-03-31 21:40:34 +0800

    the functionality is already Implemented using Arduino_Controller

--- controller/arduino_controller.py
+++ controller/arduino_controller.py
@@ -4,23 +4,25 @@ port = 'COM4'
 board = Arduino(port, baudrate=9600)
 iterator = util.Iterator(board)
 iterator.start()
+
 led_pin = board.get_pin('d:13:o')
 relay_pin = board.get_pin('d:4:o')
 electric_fan = board.get_pin('d:7:o')
 
-# Variable to keep track of whether 5 has been detected previously
-five_detected = False
 
 def control(total):
-    global five_detected
     
-    if total == 5:
-        if not five_detected:
-            led_pin.write(1)
-            five_detected = True
-        else:
-            led_pin.write(0)
-            five_detected = False
+    if total == 0:
+        led_pin.write(1)
+    elif total == 1:
+        led_pin.write(0)
+    elif total == 2:
+        electric_fan.write(1)
+    elif total == 3:
+        electric_fan.write(0)
+    elif total == 4:
+        relay_pin.write(1)
+    elif total == 5:
+        relay_pin.write(0)
     else:
-        if not five_detected:
-            led_pin.write(1)
+        print("Invalid")
