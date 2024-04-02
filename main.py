e32043c02292d4100a1d418fcc267b55ffb0e522
Dharyl <programmingdha@gmail.com>
2024-04-03 06:56:59 +0800

    Change the UI

--- main.py
+++ main.py
@@ -6,32 +6,40 @@ class App(tk.Tk):
         super().__init__()
         self.title("Hand Tracker App")
         self.attributes('-fullscreen', True)
+
+        self.configure(bg="#333333")
+        self.option_add('background', '#333333')
+        self.option_add('foreground', '#333333')
+
         self.bind("<Escape>", lambda e: self.quit())
 
         self.create_frames()
 
     def create_frames(self):
-        top_frame = tk.Frame(self)
-        bottom_frame = tk.Frame(self)
+        # top_frame = tk.Frame(self, bg="#333333")
+        # bottom_frame = tk.Frame(self, bg="#333333")
 
-        camera_frame = tk.Frame(top_frame)
-        monitoring_frame = tk.Frame(bottom_frame)
+        camera_frame = tk.Frame(self, bg="#333333")
+        # monitoring_frame = tk.Frame(bottom_frame, bg="#333333")
 
         camera_frame.pack(side=tk.TOP,fill=tk.BOTH, expand=True)
-        monitoring_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        # monitoring_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-        self.panel = tk.Label(camera_frame)
+        self.app_name = tk.Label(camera_frame, text="SMART", fg="white", bg="#333333", font=("Arial", 70))
+        self.app_name.pack(expand=True)
+
+        self.panel = tk.Label(camera_frame, bg="#333333")
         self.panel.pack_propagate(0)
-        self.panel.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        self.panel.pack(side=tk.TOP)
 
         self.hand_tracker = HandTracker(self.panel)
         self.hand_tracker.start()
 
-        top_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-        bottom_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        # top_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        # bottom_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-        self.background = tk.Label(monitoring_frame, bg="red")
-        self.background.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        # self.background = tk.Label(monitoring_frame, bg="red")
+        # self.background.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
     def start_app(self):
         self.mainloop()
