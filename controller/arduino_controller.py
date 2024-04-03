951d3d9511ae847a0f9916bb86ca244195b16392
Dharyl <programmingdha@gmail.com>
2024-04-03 22:02:13 +0800

    No electric fan and some changes

--- controller/arduino_controller.py
+++ controller/arduino_controller.py
@@ -7,33 +7,30 @@ iterator.start()
 
 led_pin = board.get_pin('d:4:o')
 relay_pin = board.get_pin('d:7:o')
-electric_fan = board.get_pin('d:8:o')
+electric_fan_pin = board.get_pin('d:8:o')
+electric_fan_pin_two = board.get_pin('d:12:o')
 
-# Initialize pin states
-led_state = 0
-electric_fan_state = 0
+led_pin_state = 0
 relay_pin_state = 0
 
 def control(total):
-    global led_state, electric_fan_state, relay_pin_state
-    
-    # Handling for each total value
+    global led_pin_state, relay_pin_state, electric_fan_state
+
     if total == 0:
-        led_state = 1
+        led_pin_state = 1
     elif total == 1:
-        led_state = 0
+        led_pin_state = 0
     elif total == 2:
-        electric_fan_state = 1
+        relay_pin_state = 0
     elif total == 3:
-        electric_fan_state = 0
+        relay_pin_state = 1
     elif total == 4:
+        led_pin_state = 0
         relay_pin_state = 1
     elif total == 5:
+        led_pin_state = 1
         relay_pin_state = 0
-    else:
-        print("Invalid")
 
-    # Write the pin states
-    led_pin.write(led_state)
-    electric_fan.write(electric_fan_state)
+
+    led_pin.write(led_pin_state)
     relay_pin.write(relay_pin_state)
--- main.py
+++ main.py
@@ -17,16 +17,12 @@ class App(tk.Tk):
 
     # FUNCTION TO CREATE THE WIDGETS
     def create_frames(self):
-        # top_frame = tk.Frame(self, bg="#333333")
-        # bottom_frame = tk.Frame(self, bg="#333333")
 
         camera_frame = tk.Frame(self, bg="#333333")
-        # monitoring_frame = tk.Frame(bottom_frame, bg="#333333")
 
         camera_frame.pack(side=tk.TOP,fill=tk.BOTH, expand=True)
-        # monitoring_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-        self.app_name = tk.Label(camera_frame, text="SMART", fg="white", bg="#333333", font=("Arial", 70))
+        self.app_name = tk.Label(camera_frame, text="IntelliHome", fg="white", bg="#333333", font=("Arial", 70))
         self.app_name.pack(expand=True)
 
         self.panel = tk.Label(camera_frame, bg="#333333")
@@ -35,12 +31,6 @@ class App(tk.Tk):
 
         self.hand_tracker = HandTracker(self.panel)
         self.hand_tracker.start()
-
-        # top_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-        # bottom_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-
-        # self.background = tk.Label(monitoring_frame, bg="red")
-        # self.background.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
     def start_app(self):
         self.mainloop()
