f4aca854a25f216aa90a669dbb5ac95270212fb5
Mark Dharyl Oabel <120765336+markdhareal@users.noreply.github.com>
2024-08-13 20:38:54 -0700

    Remove electricfan pins and variables

--- controller/arduino_controller.py
+++ controller/arduino_controller.py
@@ -7,14 +7,12 @@ iterator.start()
 
 led_pin = board.get_pin('d:4:o')
 relay_pin = board.get_pin('d:7:o')
-electric_fan_pin = board.get_pin('d:8:o')
-electric_fan_pin_two = board.get_pin('d:12:o')
 
 led_pin_state = 0
 relay_pin_state = 0
 
 def control(total):
-    global led_pin_state, relay_pin_state, electric_fan_state
+    global led_pin_state, relay_pin_state
 
     if total == 0:
         led_pin_state = 1
